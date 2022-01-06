8c773c79c638957933f17acc5c0ba5dfc82f47b4
meankso <76770011+meankso@users.noreply.github.com>
2022-01-07 06:39:39 +0900

    hello1

---
+++ PrintStartv1.java
@@ -0,0 +1,9 @@
+
+public class PrintStartv1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello one");
+
+	}
+
+}
---
+++ PrintStartv2.java
@@ -0,0 +1,9 @@
+
+public class PrintStartv2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello two");
+
+	}
+
+}
